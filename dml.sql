fee5b9d49ee94a44f82ecccb3159aa9b98bad3f9
Vicki7777777 <43415417+Vicki7777777@users.noreply.github.com>
2020-06-27 01:01:06 +0800

    DDL and DML using guide
    DDL and DML using guide

--- dml.sql
+++ dml.sql
@@ -1,7 +1,10 @@
 -- Insert record
+INSERT INTO student(id,name,age,sex)
+VALUES('001','Colin','18','male');
 
 -- Revise record
-
+update student set id = 003 where id =002;
 --  Delete record
-
+delete from student  where id = 003;
 -- Search record
+select id from student;
